942014e1cd60597b901e2b9898ad0b111531172e
AkiraWin <frog7bar@gmail.com>
2023-11-29 23:59:57 +0900

    pm0

--- tests/test_tk.py
+++ tests/test_tk.py
@@ -1,18 +1,7 @@
 # import tkinter as tk
-# from tkjug.kamisato import Hall as Kamisato
-# from tkjug.useredis import kamisato_data
 # import pytest
 
 # mock redis
-
-# @pytest.fixture
-# def app():
-#     root = tk.Tk()
-#     args = kamisato_data()
-#     app_ = Kamisato(*args, master=root)
-#     yield app_
-#     app_.quit()
-#     app_.destroy()
 
 # def test_(app):
 #     assert app.hall == 'Kamisato'
